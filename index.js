8e4f3dc28611de65454fc283442cfe4c4bc5d1ad
unknown <mkv.ysk@gmail.com>
2019-04-25 09:27:49 +0300

    Signed-off-by: unknown <mkv.ysk@gmail.com>

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js');
 const Attachment = require('discord.js');
 const config = require('./config.json');
 const mysql = require('mysql');
+const async = require('async');
 const roasts = require('./roasts.json');
 const lennys = require('./lennys.json');
 const { prefix, token } = require('./config.json');
@@ -9,12 +10,17 @@ const client = new Discord.Client();
 const fs = require('fs');
 
 const admin = ["<@236541977863127041>", "<@119864531638812672>"];
+const admin_id = ["236541977863127041", "119864531638812672"];
 
 const botId = "<@555795554232303626>";
+const botIdd = "555795554232303626";
 
-const pWords = ["pls","plz","plox","plx","plis","pliz","please","pleaze"];
+const pWords = ["plz","plox","plx","plis","pliz","please","pleaze"];
 
 const pNo = ["cunt","bitch","retard","you cock","you fucker","you slow clubfooted bastard","you retarded scythe main kid","stupid stuck in gold"];
+
+
+
 
 
 function resetBot(channel) {
@@ -40,9 +46,99 @@ function removeElement(array, element) {
   }
 }
 
-function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+
+
+//					|
+// SQL functions    |
+//					V
+
+
+
+const mysqlConfig = {
+  host: "remotemysql.com",
+  user: "EdKkBxI0w1",
+  password: "2eaNt36gNS",
+  database: "EdKkBxI0w1"
+};
+
+
+class Database {
+    constructor( config ) {
+        this.connection = mysql.createConnection( config );
+    }
+    query( sql, args ) {
+        return new Promise( ( resolve, reject ) => {
+            this.connection.query( sql, args, ( err, rows ) => {
+                if ( err )
+                    return reject( err );
+                resolve( rows );
+            } );
+        } );
+    }
+    close() {
+        return new Promise( ( resolve, reject ) => {
+            this.connection.end( err => {
+                if ( err )
+                    return reject( err );
+                resolve();
+            } );
+        } );
+    }
+}
+
+
+/*function countRows()
+{	
+	var queryResult;
+	database = new Database(mysqlConfig);
+
+  	return database.query("SELECT COUNT(*) FROM userData").then(result => {
+  		queryResult = result;
+  		return database.close();
+  	}).then(() => {
+  		console.log(queryResult);
+  	});
+};
+
+async function getNewId()
+{
+	var x = await countRows();
+	console.log(x);
+	
+	
+  	con.query("SELECT COUNT(*) FROM userData", function (err, result, fields) 
+  	{
+  		if(err) {
+	    	throw err;
+	  	} else {
+	    	console.log(result);
+	  	}
+  	});
+  	
+  	
+};*/
+
+
+
+
+
+
+
+
+
+
+
+
+//					A
+// SQL functions    |
+//					|
+
 
 
 /*var dt = new Date();
@@ -58,26 +154,11 @@ seconds = (seconds < 10 ? "0" : "") + seconds;*/
 
 
 client.once('ready', () => {
-    console.log(` Ready!`);
+    console.log(`Ready!`);
 });
 
 client.login(token);
 
-var con = mysql.createConnection({
-  host: "remotemysql.com",
-  user: "EdKkBxI0w1",
-  password: "2eaNt36gNS",
-  database: "EdKkBxI0w1"
-});
-
-con.connect(function(err) {
-  	if (err){
-  		return console.error(err);
-  	}
-  	console.log(" Connected to MySQL..");
-
-		  	
-});
 
 
 
@@ -128,7 +209,7 @@ client.on('message', message => {
         
     }
     
-    var isPls = message.content.toLowerCase().includes("please") || message.content.toLowerCase().includes("pls") || message.content.toLowerCase().includes("plz") || message.content.toLowerCase().includes("pleaze") || message.content.toLowerCase().includes("plox") || message.content.toLowerCase().includes("plx") || message.content.toLowerCase().includes("plis") || message.content.toLowerCase().includes("pliz");
+    var isPls = message.content.toLowerCase().includes("please") || message.content.toLowerCase().includes("plz") || message.content.toLowerCase().includes("pleaze") || message.content.toLowerCase().includes("plox") || message.content.toLowerCase().includes("plx") || message.content.toLowerCase().includes("plis") || message.content.toLowerCase().includes("pliz");
 
     if (message.content == "urmomgay" && !message.author.bot) {
         message.channel.send('no u');
@@ -148,51 +229,13 @@ client.on('message', message => {
 
 
 
-    /*// ECONOMY
-    //    |
-    //    |
-    //    V
+    
 
-    // JSON Files
-    let userData = JSON.parse(fs.readFileSync('storage/userData.json', 'utf8'));
-
-
-    // Events
-    if (!userData[message.author.id + message.guild.id]) userData[message.author.id + message.guild.id] = {}
-    if (!userData[message.author.id + message.guild.id].money) userData[message.author.id + message.guild.id].money = 1000;
-
-    fs.writeFile('storage/userData.json', JSON.stringify(userData), (err) => {
-        if (err) console.error(err);
-    });
+	
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    //    A
-    //    |
-    //    |
-    // ECONOMY
-
-
-
-
-*/
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
+    if (!message.content.startsWith(prefix) || message.author.bot/* || message.author.id != admin_id[0]*/) return;
     
 
     const args = message.content.slice(prefix.length).split(/ +/);
@@ -204,31 +247,167 @@ client.on('message', message => {
 	// "INSERT INTO `userData` (`id`, `username`, `cash`) VALUES ('236541977863127041', 'Raven', '1000')";
 	// Creating table:
 	// "CREATE TABLE userData (id BIGINT, username VARCHAR(255), cash INT)";
+	// INSERT INTO `userData`(`id`, `discord_id`, `username`, `cash`, `is_admin`, `peepee`, `hownig`, `is_bm`) VALUES ('1','236541977863127041','Raven','1000','1','7','24','1')
 
-
-    //if(!wereChecked.includes(message.author.id)){
-
-
-
-		//var sql = "SEARCH * FROM 'userData'";
-
-	  	//con.query(sql, function (err, result, fields) {
-
-	  		/*Object.keys(result).forEach(function(key) {
-      			var row = result[key];
-	  			console.log(row);
-
-    		});*/
-	  		
-
-	    	//console.log(result);
-
-	  	//});
-
+    
 
     //}
 
 
+
+
+    //SQL HANDLING
+    //  |
+    //  |
+    //  V
+
+
+
+    	
+    	
+
+    	//
+    	//   Creating an entry, if one doesn't exist
+    	//
+    	//
+    	//   Updating the entry's `username`, `is_bm`
+    	//
+    	//
+    	//   create peepee, hownig, if 0
+    	//
+
+		var is_bm;
+    	var is_bm_bool;
+    	if(message.member.roles.find(r => r.name === "big man") || message.member.roles.find(r => r.name === "big boi"))
+    	{
+    		is_bm = 1;
+    		is_bm_bool = true;
+    	}else
+    	{
+    		is_bm = 0;
+    		is_bm_bool = false;
+    	}
+    	var database = new Database(mysqlConfig);
+    	var newId;
+    	database.query(
+    		"SELECT COUNT(*) FROM `userData`"
+    	).then(result => {
+    		newId = result[0]['COUNT(*)'] + 1;
+    	}).then(() => {
+    		database.query("INSERT IGNORE INTO `userData` (`id`, `discord_id`, `cash`) VALUES " + "(" + "'" + `${newId}` + "'" + "," + "'" + `${message.author.id}` + "'" + ", '1000'" + ")");
+    	}).then(() => {
+    		database.query(
+    		"UPDATE IGNORE userData SET username='" + `${message.author.username}` + "'" + " WHERE discord_id='" + `${message.author.id}` + "'"
+	    	).then(result => {
+	    		database.query("UPDATE IGNORE userData SET is_bm='" + `${is_bm}` + "'" + " WHERE discord_id='" + `${message.author.id}` + "'");
+	    	}).then(() => {
+	    		database.query(
+    				"SELECT peepee,hownig FROM userData WHERE discord_id='" + `${message.author.id}` + "'"
+		    	).then(result => {
+		    		if(result[0].peepee == 0){
+		    			var newPeepee = getRandomInt(3, 15);
+		    			database.query(
+		    				"UPDATE IGNORE userData SET peepee='" + `${newPeepee}` + "'" + " WHERE discord_id='" + `${message.author.id}` + "'"
+		    			);
+		    		}
+
+		    		if(result[0].hownig == 0){
+		    			var newHownig = getRandomInt(1, 100);
+		    			database.query(
+		    				"UPDATE IGNORE userData SET hownig='" + `${newHownig}` + "'" + " WHERE discord_id='" + `${message.author.id}` + "'"
+		    			);
+		    		}
+		    	}).then(() => {
+		    		database.close();
+		    	});
+	    	});
+    	});
+
+    	/*if(message.guild.member(databaseTaggedUser).roles.find(r => r.name === "big boi")){
+    		console.log("Mentioned user is a Big Man");
+    	}
+    	else{
+    		console.log("Mentioned user is not a Big Man");
+    	}*/
+
+    	//
+    	//   Generate if someone is mentioned
+    	//
+
+    	if (!message.mentions.users.size){
+
+    	}else{
+    		var databaseTaggedUser = message.mentions.users.first();
+
+	    	//
+	    	//   Creating an entry, if one doesn't exist
+	    	//
+	    	//
+	    	//   Updating the entry's `username`, `is_bm`
+	    	//
+	    	//
+	    	//   create peepee, hownig, if 0
+	    	//
+	    		var is_bm1;
+		    	var is_bm_bool1;
+		    	if(message.guild.member(databaseTaggedUser).roles.find(r => r.name === "big man") || message.guild.member(databaseTaggedUser).roles.find(r => r.name === "big boi"))
+		    	{
+		    		is_bm1 = 1;
+		    		is_bm_bool1 = true;
+		    	}else
+		    	{
+		    		is_bm1 = 0;
+		    		is_bm_bool1 = false;
+		    	}
+
+	    	if(databaseTaggedUser.id == botIdd){
+
+	    	}else{
+
+		    	var database7 = new Database(mysqlConfig);
+		    	database7.query(
+		    		"SELECT COUNT(*) FROM `userData`"
+		    	).then(result => {
+		    		newId = result[0]['COUNT(*)'] + 1;
+		    	}).then(() => {
+		    		database7.query("INSERT IGNORE INTO `userData` (`id`, `discord_id`, `cash`) VALUES " + "(" + "'" + `${newId}` + "'" + "," + "'" + `${databaseTaggedUser.id}` + "'" + ", '1000'" + ")");
+		    	}).then(() => {
+		    		database7.query(
+		    		"UPDATE IGNORE userData SET username='" + `${databaseTaggedUser.username}` + "'" + " WHERE discord_id='" + `${databaseTaggedUser.id}` + "'"
+			    	).then(result => {
+			    		database7.query("UPDATE IGNORE userData SET is_bm='" + `${is_bm1}` + "'" + " WHERE discord_id='" + `${databaseTaggedUser.id}` + "'");
+			    	}).then(() => {
+			    		database7.query(
+		    				"SELECT peepee,hownig FROM userData WHERE discord_id='" + `${databaseTaggedUser.id}` + "'"
+		    			).then(result => {
+		    				if(result[0].peepee == 0){
+			    				var newPeepee = getRandomInt(3, 15);
+			    				database7.query(
+			    					"UPDATE IGNORE userData SET peepee='" + `${newPeepee}` + "'" + " WHERE discord_id='" + `${databaseTaggedUser.id}` + "'"
+			    				);
+		    				}
+
+				    		if(result[0].hownig == 0){
+				    			var newHownig = getRandomInt(1, 100);
+				    			database7.query(
+				    				"UPDATE IGNORE userData SET hownig='" + `${newHownig}` + "'" + " WHERE discord_id='" + `${databaseTaggedUser.id}` + "'"
+				    			);
+				    		}
+				    	}).then(() => {
+				    		database7.close();
+				    	});
+			    	});
+		    	});		    	
+		    	
+		    	
+		    	
+	    	}
+    	};
+
+    //  A
+    //  |
+    //  |
+    //SQL HANDLING
 
 
 
@@ -239,9 +418,9 @@ client.on('message', message => {
 
     /*command template:
 
-      case (""):
-            
-            break;
+	case (""):
+
+		break;
 
     */
 
@@ -253,6 +432,205 @@ client.on('message', message => {
             message.channel.send('Pong motherfucker'); 
             
             break;
+
+        case ("bank"):
+
+        	var database3 = new Database(mysqlConfig);
+        	database3.query(
+        		"SELECT cash FROM userData WHERE discord_id='" + `${message.author.id}` + "'"
+        	).then(result => {
+        		var currentCash = result[0].cash;
+        		message.channel.send(`${currentCash} Big Coins are present on your bank account`)
+        	}).then(() => {
+        		database3.close();
+        	});
+
+		break;
+
+		case ("peepee"):
+
+			var database4 = new Database(mysqlConfig);
+
+			if (!message.mentions.users.size){
+				database4.query(
+					"SELECT peepee FROM userData WHERE discord_id='" + `${message.author.id}` + "'"
+				).then(result => {
+					var yourPeepee = result[0].peepee;
+					var peepeeString = "8";
+					for (var ii = 0; ii < yourPeepee; ii++) { 
+	  				peepeeString += "="
+					}
+					peepeeString += "D";
+					message.reply(`here's your peepee \n${peepeeString}`)
+				}).then(() => {
+					database4.close();
+				});
+			}else{
+				const taggedUser = message.mentions.users.first();
+
+                if(taggedUser == botId){
+                    return message.reply("ask your mom")
+                }
+
+                database4.query(
+					"SELECT peepee FROM userData WHERE discord_id='" + `${taggedUser.id}` + "'"
+				).then(result => {
+					var yourPeepee = result[0].peepee;
+					var peepeeString = "8";
+					for (var ii = 0; ii < yourPeepee; ii++) { 
+	  				peepeeString += "="
+					}
+					peepeeString += "D";
+					message.reply(`here's ${taggedUser}'s peepee \n${peepeeString}`)
+				}).then(() => {
+					database4.close();
+				});
+
+			}
+
+		break;
+
+		case ("hownig"):
+
+            var database6 = new Database(mysqlConfig);
+
+            
+
+            if (!message.mentions.users.size){
+
+            	database6.query(
+            		"SELECT hownig FROM userData WHERE discord_id='" + `${message.author.id}` + "'"
+            	).then(result => {
+            		var response;
+            		if(result[0].hownig <= 40){
+            			response = "good job! :thumbsup:"
+            		}else{
+            			response = "sorry 🤷"
+            		}
+            		message.reply(`you are ${result[0].hownig}% nigger, ${response}`);
+            	}).then(() => {
+            		database6.close();
+            	});
+                
+
+            }
+            else{
+                const taggedUser = message.mentions.users.first();
+
+                if(taggedUser == botId){
+                    return message.channel.send("I'm 100% nigga")
+                }
+
+                database6.query(
+            		"SELECT hownig FROM userData WHERE discord_id='" + `${taggedUser.id}` + "'"
+            	).then(result => {
+            		var response;
+            		if(result[0].hownig <= 40){
+            			response = "good job! :thumbsup:"
+            		}else{
+            			response = "sorry 🤷"
+            		}
+            		message.channel.send(`${taggedUser}, you are ${result[0].hownig}% nigger, ${response}`);
+            	}).then(() => {
+            		database6.close();
+            	});
+
+                
+
+            }
+
+            break;
+
+		case ("changepeepee"):
+
+			if(message.member.roles.find(r => r.name === "big man")){
+				if (!args.length) {
+                	return message.reply("you didn't provide any arguments");
+            	}
+
+				var database5 = new Database(mysqlConfig);
+				if(!message.mentions.users.size){
+					if(typeof args[0] == 'integer'){
+						return message.reply("that's not a number dumbass")
+					}
+					if(args[0] < 1 || args[0] > 15){
+						return message.reply("wrong peepee size, minimum is 1, maximum is 15");
+					}
+					database5.query(
+						"UPDATE IGNORE userData SET peepee='" + `${args[0]}` + "'" + " WHERE discord_id='" + `${message.author.id}` + "'"
+					).then(() => {
+						message.reply(`successfuly changed your peepee size :wink:`);
+						database5.close();
+					});
+				}else{
+					if(typeof args[0] == 'integer'){
+						return message.reply("that's not a number dumbass")
+					}
+					if(args[1] < 1 || args[1] > 15){
+						return message.reply("wrong peepee size, minimum is 1, maximum is 15");
+					}
+					
+					const taggedUser = message.mentions.users.first();
+					database5.query(
+						"UPDATE IGNORE userData SET peepee='" + `${args[1]}` + "'" + " WHERE discord_id='" + `${taggedUser.id}` + "'"
+					).then(() => {
+						message.reply(`successfuly changed ${taggedUser}'s peepee size :wink:`);
+						database5.close();
+					});
+
+				}
+			}else{
+				message.reply("you are not a Big Man")
+			}
+
+		break;
+
+		case ("changenig"):
+
+			if(message.member.roles.find(r => r.name === "big man")){
+				if (!args.length) {
+                	return message.reply("you didn't provide any arguments");
+            	}
+
+				var database5 = new Database(mysqlConfig);
+				if(!message.mentions.users.size){
+					if(typeof args[0] == 'integer'){
+						return message.reply("that's not a number dumbass")
+					}
+					if(args[0] < 1 || args[0] > 100){
+						return message.reply("wrong number, minimum is 1, maximum is 100");
+					}
+					database5.query(
+						"UPDATE IGNORE userData SET hownig='" + `${args[0]}` + "'" + " WHERE discord_id='" + `${message.author.id}` + "'"
+					).then(() => {
+						message.reply(`successfuly changed your nigrate :monkey:`);
+						database5.close();
+					});
+				}else{
+					if(typeof args[0] == 'integer'){
+						return message.reply("that's not a number dumbass")
+					}
+					if(args[1] < 1 || args[1] > 100){
+						return message.reply("wrong number, minimum is 1, maximum is 100");
+					}
+					
+					const taggedUser = message.mentions.users.first();
+					database5.query(
+						"UPDATE IGNORE userData SET hownig='" + `${args[1]}` + "'" + " WHERE discord_id='" + `${taggedUser.id}` + "'"
+					).then(() => {
+						message.reply(`successfuly changed ${taggedUser}'s nigrate :monkey:`);
+						database5.close();
+					});
+
+				}
+			}else{
+				message.reply("you are not a Big Man")
+			}
+
+		break;
+		//if(message.member.roles.find(r => r.name === "big man"))
+
+		
 
         case ("avatar"):
             
@@ -340,31 +718,6 @@ client.on('message', message => {
             var randomLenny = lennys[Math.floor(Math.random()*lennys.length)];
 
             return message.channel.send(randomLenny);
-
-            break;
-
-        case ("hownig"):
-
-            var randomInt = getRandomInt(100);
-            var randomIntBM = getRandomInt(1000);
-
-            
-
-            if (!message.mentions.users.size){
-
-                return message.reply(`you are ${randomInt}% nigger, sorry 🤷`);
-
-            }
-            else{
-                const taggedUser = message.mentions.users.first();
-
-                if(taggedUser == botId){
-                    return message.channel.send("I'm 100% nigga")
-                }
-
-                return message.channel.send(`${taggedUser}, you are ${randomInt}% nigger, sorry 🤷`);
-
-            }
 
             break;
 
