c3766470f68d51f669c1297e1f5c1568720e94d7
Jamie Stivala <jamiestivala@gmail.com>
2020-04-10 22:12:52 +0200

    PrintRoasts now uses database and not json

--- src/commands/general/PrintRoast.ts
+++ src/commands/general/PrintRoast.ts
@@ -1,11 +1,11 @@
 import {Message} from "discord.js";
 import FormattedMessage from "../tools/FormattedMessage";
 import * as Roasts from "../../../resources/roasts.json"
+import CentralizedMiddleware from "../../middleware/CentralizedMiddleware";
 
-function main(message : Message, formattedMessage : FormattedMessage) : void{
-    let roasts = Roasts.accepted;
-
-    let randomRoast = roasts[Math.floor(Math.random()*roasts.length)];
+function main(message : Message, formattedMessage : FormattedMessage, middleware : CentralizedMiddleware) : void{
+    let roasts = middleware.roastMiddleware.roastCache.acceptedRoasts;
+    let randomRoast = roasts[Math.floor(Math.random()*roasts.length)].roast;
 
     if (!message.mentions.users.size)
         message.reply(randomRoast).catch(error => console.log(error));
