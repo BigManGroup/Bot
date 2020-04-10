8f647642ff4de4107a79a96a4446a55c7c5c1ab4
Jamie Stivala <jamiestivala@gmail.com>
2020-04-10 19:48:27 +0200

    Added pending so that we can track duplicates

--- src/database/model/Roast.ts
+++ src/database/model/Roast.ts
@@ -1,17 +1,22 @@
 export default class Roast{
-    roast : string;
-    submitted : string;
-    timestamp: Date;
+    _id : string
+    readonly roast : string;
+    readonly submitted : string;
+    readonly timestamp: Date;
     accepted : boolean;
+    pending : boolean;
 
-    constructor(roast: string, submitted: string, timestamp: Date, accepted: boolean) {
+    constructor (roast: string, submitted: string, timestamp: Date, accepted: boolean, pending : boolean) {
         this.roast = roast;
         this.submitted = submitted;
         this.timestamp = timestamp;
         this.accepted = accepted;
+        this.pending = pending;
     }
 
     static modelBuilder(object : any) : Roast{
-        return new Roast(object.roast, object.submitted, object.timestamp, object.accepted);
+        let roast = new Roast(object.roast, object.submitted, object.timestamp, object.accepted, object.pending);
+        if(object._id !== undefined) roast._id = object._id;
+        return roast;
     }
 }
