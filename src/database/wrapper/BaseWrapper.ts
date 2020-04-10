c87997a3d9529c9145c0e90b65e06813ae9115d8
Jamie Stivala <jamiestivala@gmail.com>
2020-04-10 20:05:43 +0200

    Added a BaseWrapper class to act as  a base for most transactions

---
+++ src/database/wrapper/BaseWrapper.ts
@@ -0,0 +1,12 @@
+import Saving from "../DatabaseHandler";
+import {Collection, MongoClient} from "mongodb";
+
+export default abstract class Base{
+    readonly session : MongoClient;
+    readonly collection : Collection;
+
+    protected constructor(collection : string) {
+        this.session = Saving.clientPool;
+        this.collection = this.session.db().collection(collection);
+    }
+}
