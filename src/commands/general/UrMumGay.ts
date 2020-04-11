50c36efb4cf1c686dba1c621aee4a232a69b780d
Jamie Stivala <jamiestivala@gmail.com>
2020-04-11 15:51:31 +0200

    Implemented UrMumGay

---
+++ src/commands/general/UrMumGay.ts
@@ -0,0 +1,9 @@
+import {Message} from "discord.js";
+import FormattedMessage from "../tools/FormattedMessage";
+import CentralizedMiddleware from "../../middleware/CentralizedMiddleware";
+
+function main(message : Message, formattedMessage : FormattedMessage, middleware : CentralizedMiddleware) : void{
+    message.channel.send('no u').catch(error => console.log(error));
+}
+
+export {main};
