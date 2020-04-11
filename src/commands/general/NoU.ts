b1cb0337cb2739849cc0b09ef66158b1569cebeb
Jamie Stivala <jamiestivala@gmail.com>
2020-04-11 15:51:58 +0200

    Removed whitespace

--- src/commands/general/NoU.ts
+++ src/commands/general/NoU.ts
@@ -2,7 +2,7 @@ import {Message} from "discord.js";
 import FormattedMessage from "../tools/FormattedMessage";
 import CentralizedMiddleware from "../../middleware/CentralizedMiddleware";
 
- function main(message : Message, formattedMessage : FormattedMessage, middleware : CentralizedMiddleware) : void{
+function main(message : Message, formattedMessage : FormattedMessage, middleware : CentralizedMiddleware) : void{
     message.channel.send('no u').catch(error => console.log(error));
 }
 
