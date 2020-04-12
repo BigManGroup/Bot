d8d0967d1cf152e9edc75bd4d464450b6a5f7867
Jamie Stivala <jamiestivala@gmail.com>
2020-04-13 01:01:53 +0200

    Changed the model from submitted to user, timestamp to timestampCreated and added timestampUpdated

--- src/database/model/Lenny.ts
+++ src/database/model/Lenny.ts
@@ -3,15 +3,18 @@ import {ObjectId} from 'mongodb'
 export default class Lenny{
     _id : ObjectId;
     readonly lenny : string;
-    readonly submitted : string;
-    readonly timestamp: Date;
+    readonly user : string;
+    readonly createdTimestamp: Date;
+    updatedTimestamp: Date;
     accepted : boolean;
     pending : boolean;
 
-    constructor (lenny: string, submitted: string, timestamp: Date, accepted: boolean, pending : boolean) {
+    //todo update db entry
+
+    constructor (lenny: string, submitted: string, createdTimestamp: Date, accepted: boolean, pending : boolean) {
         this.lenny = lenny;
-        this.submitted = submitted;
-        this.timestamp = timestamp;
+        this.user = submitted;
+        this.createdTimestamp = createdTimestamp;
         this.accepted = accepted;
         this.pending = pending;
     }
@@ -20,17 +23,19 @@ export default class Lenny{
         return {
             _id: this._id,
             lenny: this.lenny,
-            submitted: this.submitted,
-            timestamp: this.timestamp,
+            submitted: this.user,
+            createdTimestamp: this.createdTimestamp,
+            updatedTimestamp : this.updatedTimestamp,
             accepted: this.accepted,
             pending: this.pending
         }
     }
 
     static modelBuilder(object : any) : Lenny{
-        let lenny = new Lenny(object.lenny, object.submitted, object.timestamp, object.accepted, object.pending);
+        let lenny = new Lenny(object.lenny, object.user, object.createdTimestamp, object.accepted, object.pending);
         if(object._id !== undefined) lenny._id = new ObjectId(object._id);
         else lenny._id = new ObjectId();
+        lenny.updatedTimestamp = object.updatedTimestamp;
         return lenny;
     }
 }
--- src/database/model/Roast.ts
+++ src/database/model/Roast.ts
@@ -3,15 +3,16 @@ import {ObjectId} from 'mongodb'
 export default class Roast{
     _id : ObjectId;
     readonly roast : string;
-    readonly submitted : string;
-    readonly timestamp: Date;
+    readonly user : string;
+    readonly createdTimestamp: Date;
+    updatedTimestamp: Date;
     accepted : boolean;
     pending : boolean;
 
-    constructor (roast: string, submitted: string, timestamp: Date, accepted: boolean, pending : boolean) {
+    constructor (roast: string, submitted: string, createdTimestamp: Date, accepted: boolean, pending : boolean) {
         this.roast = roast;
-        this.submitted = submitted;
-        this.timestamp = timestamp;
+        this.user = submitted;
+        this.createdTimestamp = createdTimestamp;
         this.accepted = accepted;
         this.pending = pending;
     }
@@ -20,17 +21,19 @@ export default class Roast{
         return {
             _id: this._id,
             roast: this.roast,
-            submitted: this.submitted,
-            timestamp: this.timestamp,
+            submitted: this.user,
+            createdTimestamp: this.createdTimestamp,
+            updatedTimestamp : this.updatedTimestamp,
             accepted: this.accepted,
             pending: this.pending
         }
     }
 
     static modelBuilder(object : any) : Roast{
-        let roast = new Roast(object.roast, object.submitted, object.timestamp, object.accepted, object.pending);
+        let roast = new Roast(object.roast, object.user, object.createdTimestamp, object.accepted, object.pending);
         if(object._id !== undefined) roast._id = new ObjectId(object._id);
         else roast._id = new ObjectId();
+        roast.updatedTimestamp = object.updatedTimestamp;
         return roast;
     }
 }
