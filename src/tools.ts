8428c7843b2cabef4b89abb5f6ee8fc4c890af9c
Jamie Stivala <jamiestivala@gmail.com>
2020-04-13 18:29:41 +0200

    Removed channels from here

--- src/tools.ts
+++ src/tools.ts
@@ -3,8 +3,6 @@ import {Guild, User} from "discord.js";
 export default class Tools {
     static bigmanRole: string = "296754695295205376";
     static bigmanGuild: string = "264032838712688640";
-    static quoteSubmissionsChannel: string = "698923581832298617";
-    static quoteChannel: string = "669245652664057867";
 
     static isBigMan(guild: Guild, user: User): boolean {
         return guild.roles.cache.get(this.bigmanRole).members.has(user.id);
