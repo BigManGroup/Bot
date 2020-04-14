b9504530231823c1167223555348aa079dd24d13
Jamie Stivala <jamiestivala@gmail.com>
2020-04-14 20:09:35 +0200

    Added message in lenny for possible submissions

--- src/database/model/Lenny.ts
+++ src/database/model/Lenny.ts
@@ -5,37 +5,38 @@ export default class Lenny{
     readonly lenny : string;
     readonly user : string;
     readonly createdTimestamp: Date;
+    message: string;
     updatedTimestamp: Date;
     accepted : boolean;
     pending : boolean;
 
-    //todo update db entry
-
-    constructor (lenny: string, submitted: string, createdTimestamp: Date, accepted: boolean, pending : boolean) {
+    constructor(lenny: string, message: string, user: string, createdTimestamp: Date, accepted: boolean, pending: boolean) {
         this.lenny = lenny;
-        this.user = submitted;
+        this.message = message;
+        this.user = user;
         this.createdTimestamp = createdTimestamp;
         this.accepted = accepted;
         this.pending = pending;
     }
 
-    toString() : object{
-        return {
-            _id: this._id,
-            lenny: this.lenny,
-            submitted: this.user,
-            createdTimestamp: this.createdTimestamp,
-            updatedTimestamp : this.updatedTimestamp,
-            accepted: this.accepted,
-            pending: this.pending
-        }
-    }
-
-    static modelBuilder(object : any) : Lenny{
-        let lenny = new Lenny(object.lenny, object.user, object.createdTimestamp, object.accepted, object.pending);
-        if(object._id !== undefined) lenny._id = new ObjectId(object._id);
+    static modelBuilder(object: any): Lenny {
+        let lenny = new Lenny(object.lenny, object.message, object.user, object.createdTimestamp, object.accepted, object.pending);
+        if (object._id !== undefined) lenny._id = new ObjectId(object._id);
         else lenny._id = new ObjectId();
         lenny.updatedTimestamp = object.updatedTimestamp;
         return lenny;
+    }
+
+    toString(): object {
+        return {
+            _id: this._id,
+            lenny: this.lenny,
+            message: this.message,
+            user: this.user,
+            createdTimestamp: this.createdTimestamp,
+            updatedTimestamp: this.updatedTimestamp,
+            accepted: this.accepted,
+            pending: this.pending
+        }
     }
 }
