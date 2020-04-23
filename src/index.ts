ac9110d301d3bec47c27247231178cf3ef99aad1
Jamie Stivala <jamiestivala@gmail.com>
2020-04-23 19:25:31 +0200

    Added todo

--- src/index.ts
+++ src/index.ts
@@ -56,3 +56,10 @@ client.on('messageReactionRemove', async (messageReaction: MessageReaction, user
 
 client.login(properties.bot.token).catch(error => console.log("Error logging in; " + error));
 
+
+/*
+ASAP: TODO refine the saving structure and stop using the static method in such a bad way
+TODO refine middleware stuff and dynamic loading of cache
+        * Unload cache if a guild is not used for x amount of time for the sake of RAM
+        * Don't get all the cache at once, only get the needed cache and unload it when finished from it
+ */
