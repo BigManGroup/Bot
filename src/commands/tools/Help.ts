acca9b518000d9658c7e3164817b0532cd6bb381
Jamie Stivala <jamiestivala@gmail.com>
2020-09-07 17:17:29 +0200

    Removed random import

--- src/commands/tools/Help.ts
+++ src/commands/tools/Help.ts
@@ -1,4 +1,4 @@
-import {Message, MessageEmbed, TextChannel} from "discord.js";
+import {Message, MessageEmbed} from "discord.js";
 import FormattedMessage from "../model/FormattedMessage";
 import CentralizedMiddleware from "../../middleware/CentralizedMiddleware";
 
