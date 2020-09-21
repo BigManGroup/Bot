e6cc0aa1bc93339e912603f08f9ce695f5a27d9d
Jamie Stivala <jamiestivala@gmail.com>
2020-09-21 20:02:30 +0200

    Quotes are no longer accepted by default if you are a bigman

--- src/commands/submissions/SubmitQuote.ts
+++ src/commands/submissions/SubmitQuote.ts
@@ -1,7 +1,6 @@
 import {Message, MessageEmbed, TextChannel} from "discord.js";
 import FormattedMessage from "../model/FormattedMessage";
 import CentralizedMiddleware from "../../middleware/CentralizedMiddleware";
-import Tools from "../../tools";
 import Quote from "../../database/model/Quote";
 import {ObjectId} from "mongodb";
 import QuoteVoteHandler from "../../voting/QuoteVoteHandler";
@@ -30,40 +29,21 @@ function main(message: Message, formattedMessage: FormattedMessage, middleware: 
     let quoteText = roast.join(" ");
     //Remove the last 3 things in array (year, user, -)
 
-    let isBigMan = Tools.isBigMan(message.guild, middleware.guildMiddleware.bigmanRole, message.author.id);
+    let embed = new MessageEmbed().setAuthor(`${message.guild.member(quoteUser).displayName} - ${quoteYear}`, message.guild.member(quoteUser).user.avatarURL()).setTitle(quoteText).setDescription("awaiting approval by bigman").setFooter(`Submitted by ${message.guild.member(message.author.id).displayName}`);
+    message.reply("your quote was submitted. bigman council will review it and accept/decline it") //Sends the message that the submission has been received
+        .then(async (sentMessage) => {
+            let submissionMessage = await (<TextChannel>message.guild.channels.resolve(QuoteVoteHandler.quoteSubmissionsChannel)).send(embed) //Sends the message to submissions channel
+            await submissionMessage.react(VotingHandler.approveReaction);
+            await submissionMessage.react(VotingHandler.declineReaction);
 
-    if (isBigMan) {
-        let embed = new MessageEmbed().setAuthor(`${message.guild.member(quoteUser).displayName} - ${quoteYear}`, message.guild.member(quoteUser).user.avatarURL()).setTitle(quoteText).setFooter(`Submitted by ${message.guild.member(message.author.id).displayName}`);
-        message.reply("your submission was automatically accepted because you are a **BIGMAN**") //sends the message that the quote has been added
-            .then(async (sentMessage) => {
-                let embedMessage = await (<TextChannel>message.guild.channels.resolve(QuoteVoteHandler.quoteChannel)).send(embed)
+            let quote = new Quote(quoteText, String(quoteYear), quoteUser, message.guild.id, submissionMessage.id, message.author.id, new Date(), false, true);
+            quote._id = new ObjectId();
+            await middleware.quoteMiddleware.addQuote(quote)
 
-                let submittedQuote = new Quote(quoteText, String(quoteYear), quoteUser, message.guild.id, embedMessage.id, message.author.id, new Date(), true, false);
-                submittedQuote.updatedTimestamp = submittedQuote.submittedTimestamp;
-                submittedQuote._id = new ObjectId();
-                await middleware.quoteMiddleware.addQuote(submittedQuote); //Adds the quote to database and cache
-
-                await sentMessage.delete({timeout: 10000}) //Deletes the message to avoid bot spam
-                await message.delete(); //Deletes the user message
-            })
-            .catch(error => console.log(error));
-    } else {
-        let embed = new MessageEmbed().setAuthor(`${message.guild.member(quoteUser).displayName} - ${quoteYear}`, message.guild.member(quoteUser).user.avatarURL()).setTitle(quoteText).setDescription("awaiting approval by bigman").setFooter(`Submitted by ${message.guild.member(message.author.id).displayName}`);
-        message.reply("your quote was submitted. bigman council will review it and accept/decline it") //Sends the message that the submission has been received
-            .then(async (sentMessage) => {
-                let submissionMessage = await (<TextChannel>message.guild.channels.resolve(QuoteVoteHandler.quoteSubmissionsChannel)).send(embed) //Sends the message to submissions channel
-                await submissionMessage.react(VotingHandler.approveReaction);
-                await submissionMessage.react(VotingHandler.declineReaction);
-
-                let quote = new Quote(quoteText, String(quoteYear), quoteUser, message.guild.id, submissionMessage.id, message.author.id, new Date(), false, true);
-                quote._id = new ObjectId();
-                await middleware.quoteMiddleware.addQuote(quote)
-
-                await sentMessage.delete({timeout: 10000}) //Remove the message to avoid bot-spam
-                await message.delete();
-            })
-            .catch(error => console.log(error));
-    }
+            await sentMessage.delete({timeout: 10000}) //Remove the message to avoid bot-spam
+            await message.delete();
+        })
+        .catch(error => console.log(error));
 }
 
 
