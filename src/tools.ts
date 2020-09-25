1b786de8b0c8f76c6c1d7f6f9229f6acd7998f12
Jamie Stivala <jamiestivala@gmail.com>
2020-09-26 01:08:44 +0200

    Removed originalGuild from Tools.ts

--- src/tools.ts
+++ src/tools.ts
@@ -1,8 +1,6 @@
 import {Guild} from "discord.js";
 
 export default class Tools {
-    static readonly originalGuild: string = "264032838712688640";
-
     static isBigMan(guild: Guild, bigmanRole: string, user: string): boolean {
         if (guild.roles.cache.get(bigmanRole) === undefined) return guild.ownerID === user;
         else return guild.ownerID === user || guild.roles.cache.get(bigmanRole).members.has(user);
