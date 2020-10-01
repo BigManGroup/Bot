05a6eb2998b4bc581a2f67d21f210ea9fed9e481
Jamie Stivala <jamiestivala@gmail.com>
2020-10-01 16:07:32 +0200

    If quote channel is not found, don't submit quote

--- src/voting/QuoteVoteHandler.ts
+++ src/voting/QuoteVoteHandler.ts
@@ -18,8 +18,10 @@ export default class QuoteVoteHandler extends BaseVoteHandler {
         let quote = this.centralizedMiddleware.quoteMiddleware.getApprovedQuote(message.id);
         await message.delete();
 
-        let embed = new MessageEmbed().setAuthor(`${guild.members.cache.get(quote.quoteUser).displayName} - ${quote.quoteYear}`, guild.members.cache.get(quote.quoteUser).user.avatarURL()).setTitle(quote.quoteText).setFooter(`Submitted by ${guild.members.cache.get(quote.user).displayName}`);
-        await (<TextChannel>(await guild.channels.resolve(this.centralizedMiddleware.guildMiddleware.quoteChannel))).send(embed);
+        if (this.centralizedMiddleware.guildMiddleware.quoteChannel !== undefined) {
+            let embed = new MessageEmbed().setAuthor(`${guild.members.cache.get(quote.quoteUser).displayName} - ${quote.quoteYear}`, guild.members.cache.get(quote.quoteUser).user.avatarURL()).setTitle(quote.quoteText).setFooter(`Submitted by ${guild.members.cache.get(quote.user).displayName}`);
+            await (<TextChannel>(await guild.channels.resolve(this.centralizedMiddleware.guildMiddleware.quoteChannel))).send(embed);
+        }
     }
 
     protected async decline(message: Message): Promise<void> {
