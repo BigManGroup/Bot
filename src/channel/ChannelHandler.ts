d9ca75e40b26d9d00af9f72d51e07d3691c328c9
Jamie Stivala <jamiestivala@gmail.com>
2020-10-01 16:23:20 +0200

    onChannelDelete, decline all submissions in that channel

--- src/channel/ChannelHandler.ts
+++ src/channel/ChannelHandler.ts
@@ -9,10 +9,28 @@ export default class ChannelHandler{
     }
 
     async onChannelDelete(guildChannel : string){
-        if (this.centralizedMiddleware.guildMiddleware.insultSubmission === guildChannel) await this.centralizedMiddleware.guildMiddleware.setInsultSubmissionChannel(undefined);
-        if (this.centralizedMiddleware.guildMiddleware.roastSubmission === guildChannel) await this.centralizedMiddleware.guildMiddleware.setRoastSubmissionChannel(undefined);
-        if (this.centralizedMiddleware.guildMiddleware.quoteSubmission === guildChannel) await this.centralizedMiddleware.guildMiddleware.setQuoteSubmissionChannel(undefined);
-        if (this.centralizedMiddleware.guildMiddleware.quoteChannel === guildChannel) await this.centralizedMiddleware.guildMiddleware.setQuoteSubmissionChannel(undefined);    }
+        if (this.centralizedMiddleware.guildMiddleware.insultSubmission === guildChannel) {
+            await this.centralizedMiddleware.guildMiddleware.setInsultSubmissionChannel(undefined);
+            let insults = Array.from(this.centralizedMiddleware.insultMiddleware.insultCache.pendingInsult.values());
+            for (let i = 0; i !== insults.length; i++) await this.centralizedMiddleware.insultMiddleware.declineInsult(insults[i].message);
+        }
+
+        if (this.centralizedMiddleware.guildMiddleware.roastSubmission === guildChannel) {
+            await this.centralizedMiddleware.guildMiddleware.setRoastSubmissionChannel(undefined);
+
+            let roasts = Array.from(this.centralizedMiddleware.roastMiddleware.roastCache.pendingRoasts.values());
+            for (let i = 0; i !== roasts.length; i++) await this.centralizedMiddleware.roastMiddleware.declineRoast(roasts[i].message);
+        }
+
+        if (this.centralizedMiddleware.guildMiddleware.quoteSubmission === guildChannel) {
+            await this.centralizedMiddleware.guildMiddleware.setQuoteSubmissionChannel(undefined);
+
+            let quotes = Array.from(this.centralizedMiddleware.quoteMiddleware.quoteCache.pendingQuotes.values());
+            for (let i = 0; i !== quotes.length; i++) await this.centralizedMiddleware.quoteMiddleware.declineQuote(quotes[i].message);
+        }
+
+        if (this.centralizedMiddleware.guildMiddleware.quoteChannel === guildChannel) await this.centralizedMiddleware.guildMiddleware.setQuoteSubmissionChannel(undefined);
+    }
 
     async onCacheLoad(guild : Guild){
         if (guild.channels.cache.get(this.centralizedMiddleware.guildMiddleware.insultSubmission) === undefined) await this.centralizedMiddleware.guildMiddleware.setInsultSubmissionChannel(undefined);
