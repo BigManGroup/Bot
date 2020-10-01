861fbadbc91c69b078ae4358a6f31a7a99c26e2b
Jamie Stivala <jamiestivala@gmail.com>
2020-10-01 16:34:04 +0200

    Wrapped all the async events in a try catch so the bot does not crash the event handler in case of an error

--- src/index.ts
+++ src/index.ts
@@ -8,64 +8,104 @@ const client = new Discord.Client({partials: ['MESSAGE', 'REACTION']});
 let guildHandler: GuildHandler;
 
 client.on("ready", async () => {
-    guildHandler = new GuildHandler(client); //Init the GuildHandler
-    await guildHandler.setDefaultGuild(properties["default-guild"]); //Load the default guild
+    try {
+        guildHandler = new GuildHandler(client); //Init the GuildHandler
+        await guildHandler.setDefaultGuild(properties["default-guild"]); //Load the default guild
 
-    GuildTools.deleteGuild(client, guildHandler).catch(error => console.error("Unable to delete guild on start " + error));
+        GuildTools.deleteGuild(client, guildHandler).catch(error => console.error("Unable to delete guild on start " + error));
 
-    client.user.setPresence({activity: {name: 'with Big People!'}, status: 'online'}).catch(console.error); //Setting the bot status
-    console.log("Bot has started");
+        client.user.setPresence({activity: {name: 'with Big People!'}, status: 'online'}).catch(console.error); //Setting the bot status
+        console.log("Bot has started");
+    } catch (e) {
+        console.error("Error starting bot: " + e);
+    }
 });
 
 client.on("message", async (message) => {
-    let messageInterceptor = await guildHandler.getGuildMessageInterceptor(message.guild.id);
-    let commandHandler = await guildHandler.getGuildCommandHandler(message.guild.id);
-    if (message.author.bot || await messageInterceptor.intercepted(message, false)) return;
-    commandHandler.execute(message);
+    try {
+        let messageInterceptor = await guildHandler.getGuildMessageInterceptor(message.guild.id);
+        let commandHandler = await guildHandler.getGuildCommandHandler(message.guild.id);
+        if (message.author.bot || await messageInterceptor.intercepted(message, false)) return;
+        commandHandler.execute(message);
+    } catch (e) {
+        console.error("Error on message execution: " + e);
+    }
 });
 
 client.on("messageUpdate", async (oldMessage: Message, newMessage: Message) => {
-    if (newMessage.partial) newMessage = await newMessage.fetch();
-    if (newMessage.author.bot) return;
-    await (await guildHandler.getGuildMessageInterceptor(newMessage.guild.id)).intercepted(newMessage, true);
+    try {
+        if (newMessage.partial) newMessage = await newMessage.fetch();
+        if (newMessage.author.bot) return;
+        await (await guildHandler.getGuildMessageInterceptor(newMessage.guild.id)).intercepted(newMessage, true);
+    } catch (e) {
+        console.error("Error on message update: " + e);
+    }
 });
 
 client.on("messageDelete", async (message: Message | PartialMessage) => {
-    await (await guildHandler.getGuildVotingHandler(message.guild.id)).handleMessageDelete(await guildHandler.getGuildMiddleware(message.guild.id), message.id)
+    try {
+        await (await guildHandler.getGuildVotingHandler(message.guild.id)).handleMessageDelete(await guildHandler.getGuildMiddleware(message.guild.id), message.id)
+    } catch (e) {
+        console.error("Error on message delete: " + e);
+    }
 });
 
 
 client.on('messageReactionAdd', async (messageReaction: MessageReaction, user: User) => {
-    let votingHandler = await guildHandler.getGuildVotingHandler(messageReaction.message.guild.id);
-    await votingHandler.handleVote(messageReaction, user);
+    try {
+        let votingHandler = await guildHandler.getGuildVotingHandler(messageReaction.message.guild.id);
+        await votingHandler.handleVote(messageReaction, user);
+    } catch (e) {
+        console.error("Error on message reaction add: " + e);
+    }
 })
 
 client.on('messageReactionRemove', async (messageReaction: MessageReaction, user: User) => {
-    let votingHandler = await guildHandler.getGuildVotingHandler(messageReaction.message.guild.id);
-    await votingHandler.handleVote(messageReaction, user);
+    try {
+        let votingHandler = await guildHandler.getGuildVotingHandler(messageReaction.message.guild.id);
+        await votingHandler.handleVote(messageReaction, user);
+    } catch (e) {
+        console.error("Error on message reaction remove: " + e);
+    }
 });
 
 client.on('guildMemberAdd', async (member : GuildMember) => {
-    let defaultRoleHandler = await guildHandler.getDefaultRoleHandler(member.guild.id);
-    await defaultRoleHandler.onChannelJoin(member);
+    try {
+        let defaultRoleHandler = await guildHandler.getDefaultRoleHandler(member.guild.id);
+        await defaultRoleHandler.onChannelJoin(member);
+    } catch (e) {
+        console.error("Error on guild member add: " + e);
+    }
 });
 
 client.on('guildMemberUpdate', async (oldMember : GuildMember, newMember : GuildMember) => {
-    if(newMember.roles.cache.size < oldMember.roles.cache.size){
-        let defaultRoleHandler = await guildHandler.getDefaultRoleHandler(newMember.guild.id);
-        await defaultRoleHandler.onChannelJoin(newMember);
+    try {
+        if (newMember.roles.cache.size < oldMember.roles.cache.size) {
+            let defaultRoleHandler = await guildHandler.getDefaultRoleHandler(newMember.guild.id);
+            await defaultRoleHandler.onChannelJoin(newMember);
+        }
+    } catch (e) {
+        console.error("Error on guild member update: " + e);
     }
 })
 
 client.on('channelDelete', async (channel: GuildChannel) => {
-    if (channel.type !== "text") return;
-    let channelHandler = await guildHandler.getChannelHandler(channel.guild.id);
-    await channelHandler.onChannelDelete(channel.id);
+    try {
+        if (channel.type !== "text") return;
+        let channelHandler = await guildHandler.getChannelHandler(channel.guild.id);
+        await channelHandler.onChannelDelete(channel.id);
+    } catch (e) {
+        console.error("Error on channel delete: " + e);
+    }
 });
 
 client.on('roleDelete', async (deletedRole: Role) => {
-    if (!guildHandler.guildRoleHandler.has(deletedRole.guild.id)) await guildHandler.getRoleHandler(deletedRole.guild.id);
-    else await (await guildHandler.getRoleHandler(deletedRole.guild.id)).onRoleDelete(deletedRole);
+    try {
+        if (!guildHandler.guildRoleHandler.has(deletedRole.guild.id)) await guildHandler.getRoleHandler(deletedRole.guild.id);
+        else await (await guildHandler.getRoleHandler(deletedRole.guild.id)).onRoleDelete(deletedRole);
+    } catch (e) {
+        console.error("Error on role delete: " + e);
+    }
 });
 
 client.on('guildDelete', async (guild: Guild) => {
