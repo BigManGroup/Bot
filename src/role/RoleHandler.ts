fd8f960a94c6ccd2ee0d0127338398089cf60c00
Jamie Stivala <jamiestivala@gmail.com>
2020-10-03 17:00:52 +0200

    Fixed onStart nulling all the roles

--- src/role/RoleHandler.ts
+++ src/role/RoleHandler.ts
@@ -15,12 +15,22 @@ export default class RoleHandler {
         else if (deletedRole.id === this.centralizedMiddleware.guildMiddleware.bigmanRole) await this.centralizedMiddleware.guildMiddleware.setBigmanRole(undefined);
     }
 
-    onStart(guild: string): void {
+    async onStart(guild: string): Promise<void> {
         let objectGuild = client.guilds.cache.get(guild);
         let roles = Array.from(objectGuild.roles.cache.values());
 
-        for (let i = 0; i !== roles.length; i++) {
-            this.onRoleDelete(roles[i]).catch(error => console.error("Error deleting from database " + error));
+        let generalRoleFound = false;
+        let badManFound = false;
+        let bigmanRoleFound = false;
+
+        for (let i = 0; i !== roles.length && (badManFound == false || generalRoleFound == false); i++) {
+            if (roles[i].id === this.centralizedMiddleware.guildMiddleware.generalRole) generalRoleFound = true;
+            else if (roles[i].id === this.centralizedMiddleware.guildMiddleware.badmanRole) badManFound = true;
+            else if (roles[i].id === this.centralizedMiddleware.guildMiddleware.bigmanRole) bigmanRoleFound = true;
         }
+
+        if (!generalRoleFound) await this.centralizedMiddleware.guildMiddleware.setGeneralRole(undefined);
+        if (!badManFound) await this.centralizedMiddleware.guildMiddleware.setBadmanRole(undefined);
+        if (!bigmanRoleFound) await this.centralizedMiddleware.guildMiddleware.setBigmanRole(undefined);
     }
 }
