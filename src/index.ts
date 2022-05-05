ee7ce272e70a5c9ede86e3fe15adb48385edfe97
Jamie Stivala <jamiestivala@gmail.com>
2022-05-05 08:19:29 +0200

    Added a simple Client Login process

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log(process.env["MONGODB_URL"])
+import {Client, GatewayIntentBits, Partials} from "discord.js";
+
+const client = new Client({intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildMessageReactions], partials: [Partials.Reaction, Partials.Message]});
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on("messageCreate", async (message) => {
+    if(message.partial) await message.fetch(); //If the message is a partial, fetch the message
+
+})
+
+client.login(process.env["BOT_TOKEN"]).catch(error => console.dir("Unable to start with error: " + error, {depth: null}))
