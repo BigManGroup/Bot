60d507db0f2826714c04034b3c916f0f0ee62c78
Jamie Stivala <jamiestivala@gmail.com>
2022-05-18 18:51:57 +0200

    FormattedMessage now stores the command definition

--- backend/src/discord/commands/definitions/FormattedMessage.ts
+++ backend/src/discord/commands/definitions/FormattedMessage.ts
@@ -1,14 +1,18 @@
 import {Message} from "discord.js";
+import {CommandOptions} from "../../../database/model/guild/Guild";
+import CommandDefinition from "./CommandDefinition";
 
 export default class FormattedMessage {
     message: Message;
     prefix: string;
     contentNoPrefix: string;
+    commandDefinition : CommandDefinition;
 
 
-    constructor(message: Message, prefix: string, contentNoPrefix: string) {
+    constructor(message: Message, prefix: string, contentNoPrefix: string, commandDefinition : CommandDefinition) {
         this.message = message;
         this.prefix = prefix;
         this.contentNoPrefix = contentNoPrefix;
+        this.commandDefinition = commandDefinition;
     }
 }
