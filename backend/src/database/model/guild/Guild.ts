0f7e79eae9289845a02bb458fc3fb4853b9548c8
Jamie Stivala <jamiestivala@gmail.com>
2022-05-18 18:51:07 +0200

    IDatabaseGuild to store the DatabaseGuild information, which is then recreated into Guild object.
    Added defaults which is the defaultValue for undefined

--- backend/src/database/model/guild/Guild.ts
+++ backend/src/database/model/guild/Guild.ts
@@ -1,11 +1,13 @@
 import {ObjectId} from "mongodb";
 import BaseObject from "../../../tools/BaseObject";
+import CommandDefinition from "../../../discord/commands/definitions/CommandDefinition";
+import GuildCommandDefinition from "../../../discord/commands/definitions/GuildCommandDefinition";
 
 /**
  * Stores all the information about the guild
  * TODO Poll?
  */
-export default class Guild extends BaseObject{
+export default class Guild extends BaseObject implements IDatabaseGuild{
     _id : ObjectId;
     readonly id: string;
     enableSlashCommands : boolean;
@@ -22,15 +24,46 @@ export default class Guild extends BaseObject{
     readonly dateCreated : Date;
     lastAccessed : Date;
 
-    constructor(id: string) {
+    constructor(guild: IDatabaseGuild) {
         super();
-        this.id = id;
-        this.enableSlashCommands = false;
-        this.submissionGroups = [];
-        this.commandOptions = [];
-        this.prefixes = process.env["BOT_DEFAULT_PREFIXES"].split(" ");
-        this.dateCreated = new Date();
-        this.lastAccessed = new Date();
+        this._id = guild._id;
+        this.id = guild.id;
+
+        this.enableSlashCommands = guild.enableSlashCommands ?? defaultValues.enableSlashCommands;
+
+        this.minimumAdminRoleId = guild.minimumAdminRoleId ?? defaultValues.minimumAdminRoleId;
+        this.newMemberRoleId = guild.newMemberRoleId ?? defaultValues.newMemberRoleId;
+
+        this.badWordInformation = guild.badWordInformation ?? defaultValues.badWordInformation;
+        this.submissionGroups = guild.submissionGroups ?? defaultValues.submissionGroups;
+        this.commandOptions = guild.commandOptions ?? defaultValues.commandOptions;
+
+        this.prefixes = guild.prefixes ?? defaultValues.prefixes;
+
+        this.dateCreated = guild.dateCreated ?? new Date();
+        this.lastAccessed = guild.lastAccessed ?? new Date();
+    }
+
+    /**
+     * Returns the command definition modified for the guild
+     * NOT IN PLACE
+     * @param currentCommandDefinitions The current loaded command definitions
+     */
+    public getFormattedCommandOptions(currentCommandDefinitions: CommandDefinition[]) : CommandDefinition[]{
+        let formattedDefinition : CommandDefinition[]= [];
+
+        currentCommandDefinitions.forEach(currentCommandDefinition => {
+            let filteredOptions = this.commandOptions.find(value => currentCommandDefinition.id === value.commandId);
+            if(!filteredOptions) return formattedDefinition.push(new CommandDefinition(currentCommandDefinition)); //If not found copy the object and return
+             //If not found restart loop
+
+            let guildCommandDefinition = new GuildCommandDefinition(currentCommandDefinition);
+            guildCommandDefinition.commands.push(...filteredOptions.customCommands);
+            guildCommandDefinition.description = filteredOptions.description;
+            guildCommandDefinition.disabled = filteredOptions.disabled;
+        });
+
+        return formattedDefinition;
     }
 }
 
@@ -73,4 +106,37 @@ export class CommandOptions{
     customCommands: string[];
     description: string;
     disabled: boolean;
+}
+
+export interface IDatabaseGuild{
+    _id : ObjectId;
+    readonly id: string;
+    enableSlashCommands? : boolean;
+
+    minimumAdminRoleId?: string; //The minimum admin role
+    newMemberRoleId? : string; //The default role (given to every new member)
+
+    badWordInformation?: BadWordInformation; //Information for when someone is "bad"
+    submissionGroups? : SubmissionGroup[]; //Stores the information of different types of submission
+    commandOptions? : CommandOptions[]; //Stores command options such as if a command is disabled, custom command string, etc
+
+    prefixes?: string[]; //Stores the prefixes the bot activates with
+
+    readonly dateCreated? : Date;
+    lastAccessed? : Date;
+}
+
+let defaultValues : IDatabaseGuild = {
+    _id: undefined,
+    id: undefined,
+    enableSlashCommands: false,
+
+    minimumAdminRoleId: undefined,
+    newMemberRoleId: undefined,
+
+    badWordInformation: undefined,
+    submissionGroups: [],
+    commandOptions: [],
+
+    prefixes: process.env["BOT_DEFAULT_PREFIXES"].split(" ")
 }
